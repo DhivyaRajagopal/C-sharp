ec6a4df87375e59935e3d5655b4b531dc560f5d7
DhivyaRajagopal <dhivyamathi400@gmail.com>
2017-07-18 17:45:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static int arr_size; 
+        static int[] arr;
+
+        static void printArray(int[] ar)
+        {
+            int i;
+            arr_size= Convert.ToInt32(Console.ReadLine());
+           arr=new int[arr_size];
+            for (i = 0; i < arr_size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (i = 0; i < arr_size; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of an array");
+            printArray(arr);
+            Console.ReadKey();
+        }
+    }
+}
