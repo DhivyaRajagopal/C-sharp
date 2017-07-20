23c563f0bba355d9bf2203cddaad0bd0166b2e6f
DhivyaRajagopal <dhivyamathi400@gmail.com>
2017-07-20 11:50:16 +0530

    Add files via upload

---
+++ Methodoverload.cs
@@ -0,0 +1,27 @@
+﻿//Method overloading- same function name with differnet parameters
+
+
+
+
+using System;
+
+namespace Methodoverloading
+{
+    public class Program
+    {
+        public static int left(int a,int b) //same function name
+        {
+            return a << b;
+        }
+        public static int left(int a,int b,int c)
+        {
+            return c = a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(left(10, 2));
+            Console.WriteLine(left(10, 2,0));
+            Console.ReadKey();
+        }
+    }
+}
