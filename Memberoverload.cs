b135b277d1df8e60d5d44c2c8a2642f85a9d490d
DhivyaRajagopal <dhivyamathi400@gmail.com>
2017-07-20 11:49:21 +0530

    Add files via upload

---
+++ Memberoverload.cs
@@ -0,0 +1,27 @@
+﻿//Member(variable) overloading
+
+//same function name with differnt return type.
+
+using System;
+namespace Memberoverloading
+{
+ public class Program
+    {
+        public static int left(int a,int b)
+        {
+            return a << b;
+        }
+        public static double right(int a,int b)
+        {
+            return a >> b;
+        }
+        
+       public static void Main(string[] args)
+        {
+            Console.WriteLine(left(10, 2));
+            Console.WriteLine(right(10,2));
+            Console.ReadKey();
+
+        }
+    }
+}
