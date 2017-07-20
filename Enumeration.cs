378a11655a173412e5ff67350ac28d33ffabe9a0
DhivyaRajagopal <dhivyamathi400@gmail.com>
2017-07-20 11:54:42 +0530

    Add files via upload

---
+++ Enumeration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @enum
+{
+    class Program
+    {
+        public enum Month { Jan=1,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec}
+        static void Main(string[] args)
+        {
+            foreach (string res in Enum.GetNames(typeof(Month)))
+            {
+                Console.WriteLine(res);
+            }
+            Console.ReadKey();
+        }
+    }
+}
