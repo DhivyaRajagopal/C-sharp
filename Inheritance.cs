b7c8fe391561d7f7898240248d5dff9df7ecce27
DhivyaRajagopal <dhivyamathi400@gmail.com>
2017-07-20 11:53:55 +0530

    Add files via upload

---
+++ Inheritance.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Program
+    {
+        public void sum()
+        {
+            int c = 5, d = 10;
+            int res = c + d;
+            Console.WriteLine(res);
+        }
+    }
+    class end:Program
+    {
+        public void mul()
+        {
+            int c = 5, d = 10;
+            int res = c * d;
+            Console.WriteLine(res);
+        }
+        static void Main(String[] args)
+        {
+            end e = new end();
+            e.sum();
+            e.mul();
+            Console.ReadKey();
+        }
+    }
+
+}
