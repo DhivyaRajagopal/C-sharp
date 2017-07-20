339872180169672c279eb129365dccaf5d01a8c7
DhivyaRajagopal <dhivyamathi400@gmail.com>
2017-07-20 11:55:34 +0530

    Add files via upload

---
+++ Staticfield.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticField
+{
+    static class Program
+    {
+        
+        public static int i = 10;
+        public static void display()
+        {
+            Console.WriteLine("hello"+i);
+        }
+      
+        static void Main(string[] args)
+        {
+            i = 100;
+            Program.display();
+            Console.ReadKey();
+        }
+    }
+}
