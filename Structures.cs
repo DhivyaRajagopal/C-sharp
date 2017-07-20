434549b688ab628909162d3f4f2af8dba53aba22
DhivyaRajagopal <dhivyamathi400@gmail.com>
2017-07-20 11:56:10 +0530

    Add files via upload

---
+++ Structures.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//same as C
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structures
+{
+    class Program
+    { 
+        struct Usage
+        {
+           public  int i;
+            public Boolean j;
+
+        };
+        static void Main(string[] args)
+        {
+            Usage u;
+            u.i = 10;
+            u.j = false;
+            Console.WriteLine(u.i + " " + u.j);
+            Console.ReadKey();
+
+        }
+    }
+}
